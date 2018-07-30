496886ee00fec0921a915349b75da40c8b5d7e31
Brandon Lehmann <brandonlehmann@gmail.com>
2018-07-29 23:02:35 -0400

    Be gone evil demon...

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 # 
 # Please see the included LICENSE file for more information.
 
-rm -rf cpp csharp js objc php python java ruby
-mkdir {cpp,csharp,js,objc,php,python,java,ruby}
-protoc sources/*.proto --proto_path=sources/ --cpp_out=cpp/ --csharp_out=csharp/ --js_out=js/ --objc_out=objc/ --php_out=php/ --python_out=python/ --java_out=java/ --ruby_out=ruby/
+rm -rf cpp csharp js objc php python ruby
+mkdir {cpp,csharp,js,objc,php,python,ruby}
+protoc sources/*.proto --proto_path=sources/ --cpp_out=cpp/ --csharp_out=csharp/ --js_out=js/ --objc_out=objc/ --php_out=php/ --python_out=python/ --ruby_out=ruby/
